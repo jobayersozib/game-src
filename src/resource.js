cf5162e07c2b05ba7cf4c308888f22bc40c27c2e
jobayer sozib <zeni.sozibndc@gmail.com>
2017-04-12 11:30:34 +0000

    add all file

---
+++ src/resource.js
@@ -0,0 +1,20 @@
+var res = {
+    HelloWorld_png :"res/HelloWorld.png",
+    run_png:"res/running_character.png",
+    bg_jpg:"res/wbg.jpg",
+    list_jpg:"res/listView.jpg",
+    closeBtn:"res/dialogCloseButton.png",
+    suitCase:"res/suitcase.png",
+    powerImg:"res/power.png",
+    lightImg:"res/light.jpg",
+    plusImg:"res/plusimg.png",
+    testimg:"res/collect.png",
+    dollar:"res/dollar.png",
+    building_business:"res/building_business.png",
+    food_cart:"res/food_cart.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
